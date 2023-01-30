e3d7ed7dc1cd1165369064d41c199b0b5cb27d8e
Nikita <nicandreo@gmail.com>
2023-01-30 09:23:39 +0400

    solution

--- ProperTime/main.swift
+++ ProperTime/main.swift
@@ -7,4 +7,10 @@
 
 import Foundation
 
-
+func getProperTime(seconds: Int) -> String {
+    var hour: Int { seconds / 3600 }
+    var minute: Int { (seconds / 60) % 60 }
+    var second: Int { seconds % 60 }
+    
+    return String(format: "%02i:%02i:%02i", hour, minute, second)
+}
